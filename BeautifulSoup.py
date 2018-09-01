becabf9c468fe1dc5e188eeeab373e044b0572d2
mcatany <mcatany@hotmail.com>
2018-09-01 22:02:54 +0200

    Beautiful soup exercise

---
+++ BeautifulSoup.py
@@ -0,0 +1,20 @@
+# Beautiful Soup
+
+import urllib.parse, urllib.request, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input("Enter a URL:")
+
+html = urllib.request.urlopen(url, context = ctx).read()
+
+soup = BeautifulSoup(html,"html.parser")
+
+
+tags = soup("a")
+for tag in tags:
+    print(tag.get("href", None))
